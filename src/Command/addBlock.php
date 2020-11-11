c4ad6b23de39eb1f89466caaf401aa9bf055a20f
Diatrex <diahshalabi@hotmail.com>
2020-11-11 18:42:44 +0300

    #2657 [addblock-extension] Add ordering

--- src/Command/addBlock.php
+++ src/Command/addBlock.php
@@ -28,14 +28,32 @@ class addBlock
         $installed_modules = app('module.collection')->installed();
         $installed_modules = $installed_modules->merge(app('extension.collection')->installed());
 
-        $views = "";
+        $views = array();
         $params = $this->params;
         foreach ($installed_modules as $item) {
             if (file_exists($item->path . "/resources/views/" . $this->location . ".twig")) {
-                $views .= view($item->namespace . '::' . $this->location, compact('params'));
+                $views[] = [
+                    'order' => 10,
+                    'view' => view($item->namespace . '::' . $this->location, compact('params'))
+                ];
+            } elseif (count(glob($item->path . "/resources/views/" . $this->location . "_*.twig"))) {
+                $order = glob($item->path . "/resources/views/" . $this->location . "_*.twig")[0];
+                $location = str_replace('/', '\/', $this->location);
+                preg_match('/' . $location . '_(.*?)\.twig/', $order, $match);
+                $order = $match[1];
+                $views[] = [
+                    'order' => $order,
+                    'view' => view(
+                        $item->namespace . '::' . $this->location . '_' . $order,
+                        compact('params')
+                    )
+                ];
             }
         }
 
-        return $views;
+        $ordered = array_column($views, 'order');
+        array_multisort($ordered, SORT_ASC, $views);
+
+        return implode('', array_column($views, 'view'));
     }
 }
